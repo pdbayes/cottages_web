91888d936039b29756cc22db58e12c9247116e17
pdbayes <58221502+pdbayes@users.noreply.github.com>
2024-04-03 13:29:43 +0100

    Update info.php

--- info.php
+++ info.php
@@ -146,9 +146,14 @@ Extreme temperatures in Cornwall are particularly rare.
 	  include("access.php");
 	  ?>
 	  
-	  <?php
-	  include("footer.php");
-	  ?>
+<!-- AddThis Button BEGIN -->
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<div class="row">
+	 <div class="col-xs-12">
+	 <br>
+	 	  <div class="addthis_sharing_toolbox"></div>
+	 </div>
+</div>
 	 </div>
 	   <!-- Bootstrap core JavaScript
     ================================================== -->
