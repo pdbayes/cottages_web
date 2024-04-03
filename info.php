ea7b54136a257b90212dea8367f00ef2edb3e63e
pdbayes <58221502+pdbayes@users.noreply.github.com>
2024-04-03 13:21:38 +0100

    Update info.php

--- info.php
+++ info.php
@@ -31,9 +31,55 @@
 <!-- NAVBAR ================================================== -->
   <body>
   <div class="container">
-  	  <?php
-	  include("home_nav.php");
-	  ?>
+  	  <!-- NAVBAR ================================================== -->
+          <!-- Fixed navbar -->
+          <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Tregrill Farm Cottages</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="./index.html">Home</a></li>
+            <li><a href="./index.html#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Information <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="./info.html">Find Us</a></li>
+                <li><a href="./info.html#weather">Weather</a></li>
+                <li><a href="./info.html#holiday">Your Holiday</a></li>
+                <li><a href="./info.html#whats_on">Whats On</a></li>
+                <li><a href="info.html#access">Accessibility</a></li>
+              </ul>
+            </li>
+			<li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Things To Do <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="./things_to_do.html#walking">Walking</a></li>
+				<li><a href="./things_to_do.html#beaches">Beaches</a></li>
+				<li><a href="./things_to_do.html#gardens">Gardens and History</a></li>
+				<li><a href="./things_to_do.html#animal">Animal Attractions</a></li>
+				<li><a href="./things_to_do.html#active">Active and Adventure</a></li>
+				<li><a href="./things_to_do.html#other">Other</a></li>
+               
+              </ul>
+            </li>
+            <li><a href="./covid_info.html">Covid-19 Information</a></li>
+          </ul>
+          <!--<ul class="nav navbar-nav navbar-right">
+            <li><a href="../navbar/">Default</a></li>
+            <li><a href="../navbar-static-top/">Static top</a></li>
+            <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>
+          </ul>-->
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
   
 	  <?php
 	  include("findus.php");
