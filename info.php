487336c6696f31ec29b144b0e930c2f6ee4c6695
pdbayes <58221502+pdbayes@users.noreply.github.com>
2024-04-03 13:32:27 +0100

    Update info.php

--- info.php
+++ info.php
@@ -138,9 +138,24 @@ Extreme temperatures in Cornwall are particularly rare.
 	    <?php
 	  include("info_new.php");
 	  ?>
-	   <?php
-	  include("whats_on.php");
-	  ?>
+<div class="row">
+<div class="col-sm-12">
+<a name="whats_on"></a>
+<h1 id ="whats_on">What's on</h1>
+<div id="satnav" class ="hidden-xs hidden-sm">
+<iframe width="768" height="400px" style="border:none;" scrolling="no" vspace="0" hspace="0" frameborder="0" marginwidth="0" marginheight="0" src="https://www.visitcornwall.com/widget/whats-on">
+You need a Frames Capable browser to view this content.
+</iframe>   
+</div> 
+
+
+<div class="embed-responsive embed-responsive-4by3 visible-xs visible-sm">
+<iframe class="embed-responsive-item" style="border:none;" scrolling="yes" vspace="0" hspace="0" frameborder="0" marginwidth="0" marginheight="0" src="https://www.visitcornwall.com/widget/whats-on">
+</iframe>   
+</div>
+</div>
+</div> 
+<hr class="featurette-divider">
 	  
 <div class="row">
 <div class="col-xs-12">
